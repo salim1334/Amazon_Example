307a0f5b9258417209e9a42ad1458c6a00669385
salimahmedmuhammed98765@gmail.com <salimahmedmuhammed98765@gmail.com>
2025-05-21 15:56:46 +0300

    Carosal section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import CarouselEffect from "../../components/CarouselEffect/CarouselEffect"
 import Header from "../../components/Header/Header"
 
 function Home() {
   return (
     <div>
       <Header />
+      <CarouselEffect />
     </div>
   )
 }
